6fb28862375c71f8550153d765b997b25309c7c0
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-12-08 08:56:33 +0100

    chore: astro upgrade + bump back to react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from "astro/config"
-import preact from "@astrojs/preact"
+import react from "@astrojs/react"
 import vercel from "@astrojs/vercel/static"
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [
-    preact({
-      compat: true,
-    }),
-  ],
+  integrations: [react()],
   adapter: vercel({
     imagesConfig: {
       sizes: [180, 320, 640, 1280],
+      domains: [],
       imageService: true,
       minimumCacheTTL: 2592000,
     },
